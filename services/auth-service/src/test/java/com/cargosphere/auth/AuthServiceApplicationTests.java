47574361cd88ed6215af016e8ee1aa701976f3e2
Dnyanesh <dnyanesh.gholap21@vit.edu>
2026-07-04 01:41:49 +0530

    feat: create auth service base project

---
+++ services/auth-service/src/test/java/com/cargosphere/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cargosphere.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
