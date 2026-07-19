f5bb345799534539de663a8e61620dd039f022fd
Ajaychougule <ajaychougule17@gmail.com>
2026-07-19 17:18:09 +0530

    feat(shipment): add shipment JPA entities
    - Add Shipment entity mapped to shipment_schema.shipments
    - Add CargoDetail entity mapped to shipment_schema.cargo_details
    - Add ShipmentEvent entity mapped to shipment_schema.shipment_events
    - Add enums for shipment status, shipment type, cargo type, and shipment event type
    - Validate entity mappings against test and dev databases

---
+++ services/shipment-service/src/main/java/com/cargosphere/shipment/entity/enums/CargoType.java
@@ -0,0 +1,12 @@
+package com.cargosphere.shipment.entity.enums;
+
+public enum CargoType {
+    GENERAL,
+    FRAGILE,
+    HAZARDOUS,
+    PERISHABLE,
+    LIQUID,
+    HEAVY,
+    ELECTRONICS,
+    OTHER
+}
---
+++ services/shipment-service/src/main/java/com/cargosphere/shipment/entity/enums/ShipmentStatus.java
@@ -0,0 +1,9 @@
+package com.cargosphere.shipment.entity.enums;
+
+public enum ShipmentStatus {
+    CREATED,
+    BOOKED,
+    IN_TRANSIT,
+    DELIVERED,
+    CANCELLED
+}
