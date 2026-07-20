33c5c5613b346dbe484b67820441dc65fcf8fbdc
Dnyanesh <dnyanesh.gholap21@vit.edu>
2026-07-20 20:48:48 +0530

    feat(registry): add standalone Eureka service registry

---
+++ services/service-registry/src/test/java/com/cargosphere/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cargosphere.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ServiceRegistryApplication.class)
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
