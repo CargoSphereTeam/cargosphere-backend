fa4e468e6c55aa4a1d8905f6c1bfbd1ab5f08ad7
Dnyanesh <dnyanesh.gholap21@vit.edu>
2026-08-01 18:35:38 +0530

    fix(registry): isolate Eureka server context test

--- services/service-registry/src/test/java/com/cargosphere/registry/ServiceRegistryApplicationTests.java
+++ services/service-registry/src/test/java/com/cargosphere/registry/ServiceRegistryApplicationTests.java
@@ -3,7 +3,14 @@ package com.cargosphere.registry;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ServiceRegistryApplication.class)
+@SpringBootTest(
+        classes = ServiceRegistryApplication.class,
+        properties = {
+                "spring.autoconfigure.exclude="
+                        + "org.springframework.cloud.netflix.eureka.server."
+                        + "EurekaServerAutoConfiguration"
+        }
+)
 class ServiceRegistryApplicationTests {
 
     @Test
